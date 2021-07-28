2e235dc98fffa738d406e9cb3f0e5a8ebca7bf9e
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-07-28 19:18:31 -0300

    adiciona exercicio 2

---
+++ 01-fundamentos/bloco-04-intro-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripsts2.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+
+
+console.log(base);
+console.log(altura);
+console.log(area);
--- 01-fundamentos/bloco-04-intro-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ 01-fundamentos/bloco-04-intro-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -2,7 +2,6 @@ const myName = 'Artur';
 const birthCity = 'Belo Horizonte';
 let birthYear = 2001;
 
-
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);

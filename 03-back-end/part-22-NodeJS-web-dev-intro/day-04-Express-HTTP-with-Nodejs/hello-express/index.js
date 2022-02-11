5e5bf2bde28bdb58de0b62d0ea513be640c18ff7
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-02-11 18:53:16 -0300

    update: folder day 3 NodeJS

--- 03-back-end/part-22-NodeJS-web-dev-intro/day-04-Express-HTTP-with-Nodejs/hello-express/index.js
+++ 03-back-end/part-22-NodeJS-web-dev-intro/day-04-Express-HTTP-with-Nodejs/hello-express/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const app = express(); // 1
+const app = express();
 
-app.get('/hello', handleHelloWorldRequest); // 2
+app.get('/hello', handleHelloWorldRequest);
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
-}); // 3
+});
 
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World!'); // 4
-}
+function handleHelloWorldRequest(_req, res) {
+  res.status(200).send({msg: 'Hello World!'});
+};

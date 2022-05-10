c4d17c2a69528f1017757dd09ddf78caf246232e
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-05-10 14:19:27 -0300

    feat: implements enum for year seasons and monthsd

---
+++ 03-back-end/part-26-TypeScript/day-26.1-introduction-to-typescript/my-ts-scripts/yearMonths.ts
@@ -0,0 +1,16 @@
+enum months {
+  JANUARY = "january",
+  FEBRUARY = "february",
+  MARCH = "march",
+  APRIL = "april",
+  MAY = "may",
+  JUNE = "june",
+  JULY = "july",
+  AUGUST = "august",
+  SEPTEMBER = "sepember",
+  OCTOBER = "october",
+  NOVEMBER = "november",
+  DECEMBER = "december",
+}
+
+export default months;
---
+++ 03-back-end/part-26-TypeScript/day-26.1-introduction-to-typescript/my-ts-scripts/yearSeasons.ts
@@ -0,0 +1,8 @@
+enum seasons {
+  SUMMER = "summer",
+  AUTUMN = "autum",
+  WINTER = "winter",
+  SPRING = "spring",
+}
+
+export default seasons;
